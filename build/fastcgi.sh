b09af824c3475430ce84d04f5752d1a089f759a9
Sebastien COUDERT <sebastien.coudert@ganil.fr>
2021-06-29 09:09:32 +0100

    ligthhpd: fast-CGI

---
+++ build/fastcgi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#LD_LIBRARY_PATH="/home/pi/SC/CppCMS/cppcms/build;/home/pi/SC/CppCMS/cppcms/build/booster;/home/pi/SC/libI2C/" /home/pi/SC/CppI2C/fake-web --config=/home/pi/SC/CppI2C/config.js
+
+/home/pi/SC/CppCMS/cppcms/build/hello_world -c /home/pi/SC/CppCMS/cppcms/examples/hello_world/config_fastcgi.js
+
